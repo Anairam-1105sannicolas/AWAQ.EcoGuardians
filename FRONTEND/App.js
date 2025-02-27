90006b71cf705fcec3a3893d2fb1f9ecaabc2f84
Anairam-1105sannicolas <142516838+Anairam-1105sannicolas@users.noreply.github.com>
2025-02-26 21:56:51 -0600

    Add files via upload

---
+++ FRONTEND/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src="https://ecoguardians2-0.github.io/EcoGuardians-2-0/" 
+            width="800" 
+            height="600" 
+            frameborder="0" 
+            allowfullscreen>
+    </iframe>
+    </div>
+  );
+}
+
+export default App;
